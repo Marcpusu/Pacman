ca2ad53b4d5d1b0e60c35b954b9402a1bfcfe75c
marcpusu <marcpuig94@gmail.com>
2019-05-06 16:24:34 +0200

    Main form created. Pacman movement functions added.

---
+++ Pacman/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Pacman
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbPacman = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPacman)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbPacman
+            // 
+            this.pbPacman.Location = new System.Drawing.Point(339, 210);
+            this.pbPacman.Name = "pbPacman";
+            this.pbPacman.Size = new System.Drawing.Size(25, 25);
+            this.pbPacman.TabIndex = 0;
+            this.pbPacman.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbPacman);
+            this.Name = "MainForm";
+            this.Text = "Pacman";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPacman)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbPacman;
+    }
+}
+
---
+++ Pacman/MainForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pacman
+{
+    public partial class MainForm : Form
+    {
+        #region Global Vars
+        const int iSpeed = 25;
+        const int iDistance = 2;
+
+        Direction oDirection;
+        Direction oPreviousDirection;
+        Direction oDirectionPressed;
+
+        Timer timer = new Timer();
+        List<Panel> lstWalls = new List<Panel>();
+
+        private enum Direction
+        {
+            None = 0,
+            Up = 1,
+            Down = 2,
+            Left = 3,
+            Right = 4
+        }
+        #endregion
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            timer.Interval = iSpeed;
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Start();
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            oPreviousDirection = oDirection;
+
+            if (e.KeyCode == Keys.Up)
+                oDirectionPressed = Direction.Up;
+            else if (e.KeyCode == Keys.Down)
+                oDirectionPressed = Direction.Down;
+            else if (e.KeyCode == Keys.Left)
+                oDirectionPressed = Direction.Left;
+            else if (e.KeyCode == Keys.Right)
+                oDirectionPressed = Direction.Right;
+        }
+
+        private void MainForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            oDirection = oPreviousDirection;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            oDirection = oDirectionPressed;
+
+            if (Blocked(oDirection))
+            {
+                oDirection = oPreviousDirection;
+                if (Blocked(oPreviousDirection))
+                    return;
+            }
+
+            switch (oDirection)
+            {
+                case Direction.Up:
+                    if (oPreviousDirection == Direction.Down)
+                        oPreviousDirection = Direction.None;
+                    pbPacman.Top -= iDistance;
+                    break;
+                case Direction.Down:
+                    if (oPreviousDirection == Direction.Up)
+                        oPreviousDirection = Direction.None;
+                    pbPacman.Top += +iDistance;
+                    break;
+                case Direction.Left:
+                    if (oPreviousDirection == Direction.Right)
+                        oPreviousDirection = Direction.None;
+                    pbPacman.Left -= iDistance;
+                    break;
+                case Direction.Right:
+                    if (oPreviousDirection == Direction.Left)
+                        oPreviousDirection = Direction.None;
+                    pbPacman.Left += iDistance;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private bool Blocked(Direction direction)
+        {
+            bool bBlocked = false;
+
+            Rectangle r = new Rectangle(new Point(pbPacman.Location.X, pbPacman.Location.Y), pbPacman.Size);
+
+            switch (direction)
+            {
+                case Direction.Up:
+                    r.Location = new Point(pbPacman.Location.X, pbPacman.Location.Y - iDistance);
+                    break;
+                case Direction.Down:
+                    r.Location = new Point(pbPacman.Location.X, pbPacman.Location.Y + iDistance);
+                    break;
+                case Direction.Left:
+                    r.Location = new Point(pbPacman.Location.X - iDistance, pbPacman.Location.Y);
+                    break;
+                case Direction.Right:
+                    r.Location = new Point(pbPacman.Location.X + iDistance, pbPacman.Location.Y);
+                    break;
+                default:
+                    break;
+            }
+
+            foreach (Panel x in lstWalls)
+            {
+                if (r.IntersectsWith(x.Bounds))
+                {
+                    bBlocked = true;
+                    break;
+                }
+            }
+
+            return bBlocked;
+        }
+    }
+}
